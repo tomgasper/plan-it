807f121e3691c0237797ecb00bbf9887d50c78c7
tomek <tomgasper01@gmail.com>
2024-09-16 13:14:13 +0200

    Add option to delete project task from the project settings page

--- clients/plan-it-web/src/components/Project/Project.tsx
+++ clients/plan-it-web/src/components/Project/Project.tsx
@@ -7,6 +7,7 @@ import { ProjectSettings } from './ProjectSettings';
 import { ExtendedModal } from '../Common/ExtendedModal';
 import { User } from '../../types/User';
 import { ProjectWithDetails } from '../../types/Project';
+import { ProjectAssignedUsers } from './ProjectAssignedUsers';
 
 interface ProjectProps {
     handleProps : React.HTMLAttributes<unknown> | undefined;
@@ -20,43 +21,6 @@ export function Project({ onUpdate, onRemove, handleProps, id, project } : Proje
     // use app logic not server side
     // const { data: project, error: projectError, isLoading: projectLoading, refetch } = useGetProjectWithDetailsQuery(id);
     const [modalOpened, { open, close }] = useDisclosure(false);
-
-    const getUsersFromAllTasks = (maxNumOfUsers = 5) : User[] => {
-        if (!project?.projectTasks) return [];
-
-        const users : User[] = [];
-        const userIds = new Set<string>(); 
-    
-        for (const task of project.projectTasks) {
-            if (task.taskWorkers === null) continue;
-            for (const user of task.taskWorkers) {
-                if (userIds.size >= maxNumOfUsers) break;
-                if (userIds.has(user.id)) continue;
-                userIds.add(user.id);
-                users.push(user);
-            }
-        }
-
-        return users;
-    }
-
-    const numOfUsersAssignedToTasks = () : number => {
-        if (!project?.projectTasks) return 0;
-
-        const users = new Set<string>();
-        for (const task of project.projectTasks) {
-            if (task.taskWorkers === null) continue;
-            for (const user of task.taskWorkers) {
-                users.add(user.id);
-            }
-        }
-
-        return users.size;
-    }
-
-    const MAX_NUM_AVATARS_TO_DISPLAY = 5;
-    const users = getUsersFromAllTasks(MAX_NUM_AVATARS_TO_DISPLAY);
-    const restNumOfUsers = numOfUsersAssignedToTasks() - users.length;
 
   return (
     <>
@@ -74,19 +38,14 @@ export function Project({ onUpdate, onRemove, handleProps, id, project } : Proje
             </Group>
         </Group>
         <Progress autoContrast value={(0 * 100)} mt={5} />
-        <Group justify="space-between">
+        <Group justify="space-between" align='center'>
         <Text fz="sm" mt="md">
         Tasks completed:{' '}
             <Text span fw={500} c="bright">
                 {project.projectTasks.filter(task => task.isCompleted).length}/{project.projectTasks.length}
             </Text>
         </Text>
-        <Avatar.Group>
-            {users.map((user : User) => (
-                <Avatar key={user.id} src={user.avatarUrl} radius="xl" />
-            ))}
-            { restNumOfUsers > 0 && <Avatar radius="xl">{restNumOfUsers}</Avatar>}
-        </Avatar.Group>
+            <ProjectAssignedUsers project={project} />
         </Group>
     </Stack>
     </>
